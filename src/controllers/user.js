1f7ea875d27712484bd1dcfbf918801d3b513203
John Xander <johnblox5@gmail.com>
2022-09-14 16:42:55 +0100

    feat: profileImgUrl added to schema

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,6 +4,8 @@ import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
 export const create = async (req, res) => {
   const userToCreate = await User.fromJson(req.body)
 
+  console.log(userToCreate)
+
   try {
     const existingUser = await User.findByEmail(userToCreate.email)
 
