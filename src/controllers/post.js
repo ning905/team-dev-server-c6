ec447e5c17b401f750120ce9162e08596c659939
miresque <mirkilinc@gmail.com>
2022-09-14 09:53:34 +0100

    feat:_app_api_post_entry_functionality

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,13 +1,29 @@
-import { sendDataResponse } from '../utils/responses.js'
+import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
+import { PrismaClient } from '@prisma/client'
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '../utils/config.js'
+const prisma = new PrismaClient()
 
 export const create = async (req, res) => {
   const { content } = req.body
+  const token = req.headers.authorization.split(' ')
 
   if (!content) {
-    return sendDataResponse(res, 400, { content: 'Must provide content' })
+    return sendMessageResponse(res, 400, 'Must provide content')
   }
 
-  return sendDataResponse(res, 201, { post: { id: 1, content } })
+  try {
+    const user = jwt.verify(token[1], JWT_SECRET)
+    const createdPost = await prisma.post.create({
+      data: {
+        content,
+        userId: user.userId
+      }
+    })
+    return sendDataResponse(res, 201, { post: createdPost })
+  } catch (err) {
+    return sendMessageResponse(res, 500, err.message)
+  }
 }
 
 export const getAll = async (req, res) => {
