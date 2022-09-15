5c5f7626c1dd4966e86f1c32db16b9ed9f5a2919
Michele M <mnmin@gmail.com>
2022-09-15 13:47:51 +0100

    added delete post

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,5 +1,6 @@
 import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
 import { PrismaClient } from '@prisma/client'
+import dbClient from '../utils/dbClient.js'
 const prisma = new PrismaClient()
 
 export const create = async (req, res) => {
@@ -29,7 +30,7 @@ export const getAll = async (req, res) => {
     skip: 0,
     take: 100,
     orderBy: {
-      createdAt: 'asc'
+      createdAt: 'desc'
     },
     include: {
       user: {
@@ -44,4 +45,46 @@ export const getAll = async (req, res) => {
     }
   })
   return sendDataResponse(res, 200, posts)
+}
+
+export const deletePost = async (req, res) => {
+  // GET the post ID we want to delete from req.param
+  // Convert post id from string to number
+  const id = Number(req.params.id)
+  console.log('id', id)
+  // get the post from database
+  try {
+    const foundPost = await dbClient.post.findUnique({
+      where: {
+        id
+      },
+      include: {
+        user: true
+      }
+    })
+    console.log('found Post', foundPost)
+    // if no post, throw error
+    if (!foundPost) {
+      return sendMessageResponse(res, 404, 'Error in retriving post')
+    }
+    // if post user id does not match request user id, throw error
+
+    if (foundPost.user.id !== req.user.id) {
+      return sendMessageResponse(
+        res,
+        403,
+        'Request authorization to delete post'
+      )
+    }
+    // if all good, call deletepost on prisma
+    const deletePost = await dbClient.post.delete({
+      where: {
+        id
+      }
+    })
+    console.log('delete post', deletePost)
+    return sendDataResponse(res, 201, deletePost)
+  } catch (err) {
+    sendMessageResponse(res, 500, 'Unable to delete post')
+  }
 }
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { create, getAll } from '../controllers/post.js'
+import { create, getAll, deletePost } from '../controllers/post.js'
 import { validateAuthentication } from '../middleware/auth.js'
 
 const router = Router()
 
 router.post('/', validateAuthentication, create)
 router.get('/', validateAuthentication, getAll)
+router.delete('/:id', validateAuthentication, deletePost)
 
 export default router
