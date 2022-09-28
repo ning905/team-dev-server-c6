e46540356aeca910bf05afe349307003f22a337b
Ning Ma <ningma905@gmail.com>
2022-09-28 19:56:57 +0100

    feat: add eventEmitter

---
+++ src/eventEmitter/index.js
@@ -0,0 +1,48 @@
+const EventEmitter = require('events')
+const {
+  createCohortCreatedEvent,
+  createRegisterEvent,
+  createUpdateEmailEvent,
+  createUpdatePrivacyEvent,
+  createUpdateActivateEvent,
+  createChangeRoleEvent,
+  createRenameCohortEvent,
+  createDeleteCohortEvent,
+  createAddToCohortEvent,
+  createRemoveFromCohortEvent
+} = require('./utils')
+
+class MyEventEmitter extends EventEmitter {}
+const myEmitter = new MyEventEmitter()
+
+myEmitter.on('register', (user, admin = null) =>
+  createRegisterEvent(user, (admin = null))
+)
+myEmitter.on('update-email', (user, oldEmail, admin = null) =>
+  createUpdateEmailEvent(user, oldEmail, (admin = null))
+)
+myEmitter.on('update-privacy', (user, admin = null) =>
+  createUpdatePrivacyEvent(user, (admin = null))
+)
+myEmitter.on('update-account-activate', (user, admin = null) =>
+  createUpdateActivateEvent(user, (admin = null))
+)
+myEmitter.on('change-role', (assignee, oldRole, assigner) =>
+  createChangeRoleEvent(assignee, oldRole, assigner)
+)
+
+myEmitter.on('create-cohort', (cohort, admin) =>
+  createCohortCreatedEvent(cohort, admin)
+)
+myEmitter.on('rename-cohort', (cohort, oldName, admin) =>
+  createRenameCohortEvent(cohort, oldName, admin)
+)
+myEmitter.on('delete-cohort', (cohort, admin) =>
+  createDeleteCohortEvent(cohort, admin)
+)
+myEmitter.on('add-to-cohort', (admin, student, cohort) =>
+  createAddToCohortEvent(admin, student, cohort)
+)
+myEmitter.on('add-to-cohort', (admin, student, cohort) =>
+  createRemoveFromCohortEvent(admin, student, cohort)
+)
