830ab44c165589dbb24bd1622e26586f6b9c64db
John Xander <johnblox5@gmail.com>
2022-09-29 09:50:13 +0100

    feat: exercises

---
+++ src/routes/exercise.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import {
+    createExercise,
+    getAllExercises,
+    deleteExercise,
+    getExerciseById
+} from '../controllers/exercise.js'
+
+const router = Router()
+
+router.post('/', createExercise)
+router.get('/', getAllExercises)
+router.delete('/:id', deleteExercise)
+router.get('/:id', getExerciseById)
+
+export default router
