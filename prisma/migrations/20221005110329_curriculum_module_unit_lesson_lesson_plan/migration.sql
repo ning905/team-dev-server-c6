6f516f11491495283a1bb411174294522f2c1b20
Michele M <mnmin@gmail.com>
2022-10-05 12:03:54 +0100

    migration curriculum module unit lesson lesson plan

---
+++ prisma/migrations/20221005110329_curriculum_module_unit_lesson_lesson_plan/migration.sql
@@ -0,0 +1,116 @@
+-- CreateTable
+CREATE TABLE "Curriculum" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Curriculum_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Module" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "description" TEXT,
+    "objectives" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Module_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Unit" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "description" TEXT,
+    "objectives" TEXT[],
+    "moduleId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Unit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Lesson" (
+    "id" SERIAL NOT NULL,
+    "date" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "description" TEXT,
+    "objectives" TEXT[],
+    "unitId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Lesson_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "LessonPlan" (
+    "id" SERIAL NOT NULL,
+    "description" TEXT,
+    "objectives" TEXT[],
+    "lessonId" INTEGER NOT NULL,
+    "createdById" INTEGER NOT NULL,
+    "createdForId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "LessonPlan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ExerciseToLesson" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_CurriculumToModule" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "LessonPlan_lessonId_key" ON "LessonPlan"("lessonId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ExerciseToLesson_AB_unique" ON "_ExerciseToLesson"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ExerciseToLesson_B_index" ON "_ExerciseToLesson"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CurriculumToModule_AB_unique" ON "_CurriculumToModule"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CurriculumToModule_B_index" ON "_CurriculumToModule"("B");
+
+-- AddForeignKey
+ALTER TABLE "Unit" ADD CONSTRAINT "Unit_moduleId_fkey" FOREIGN KEY ("moduleId") REFERENCES "Module"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Lesson" ADD CONSTRAINT "Lesson_unitId_fkey" FOREIGN KEY ("unitId") REFERENCES "Unit"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "LessonPlan" ADD CONSTRAINT "LessonPlan_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "LessonPlan" ADD CONSTRAINT "LessonPlan_createdForId_fkey" FOREIGN KEY ("createdForId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "LessonPlan" ADD CONSTRAINT "LessonPlan_lessonId_fkey" FOREIGN KEY ("lessonId") REFERENCES "Lesson"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ExerciseToLesson" ADD CONSTRAINT "_ExerciseToLesson_A_fkey" FOREIGN KEY ("A") REFERENCES "Exercise"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ExerciseToLesson" ADD CONSTRAINT "_ExerciseToLesson_B_fkey" FOREIGN KEY ("B") REFERENCES "Lesson"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CurriculumToModule" ADD CONSTRAINT "_CurriculumToModule_A_fkey" FOREIGN KEY ("A") REFERENCES "Curriculum"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CurriculumToModule" ADD CONSTRAINT "_CurriculumToModule_B_fkey" FOREIGN KEY ("B") REFERENCES "Module"("id") ON DELETE CASCADE ON UPDATE CASCADE;
