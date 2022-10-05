cf7bfce89e1a419223963ca7b8a033cc729a8796
Ning Ma <ningma905@gmail.com>
2022-10-05 15:51:18 +0100

    refactor: catch error

--- src/controllers/curriculum.js
+++ src/controllers/curriculum.js
@@ -1,8 +1,16 @@
-import { sendDataResponse } from '../utils/responses.js'
+import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
 import dbClient from '../utils/dbClient.js'
+import { ServerErrorEvent } from '../eventEmitter/utils.js'
+import { myEmitter } from '../eventEmitter/index.js'
 
 export const getAllCurriculums = async (req, res) => {
-  const currs = await dbClient.curriculum.findMany()
+  try {
+    const currs = await dbClient.curriculum.findMany()
 
-  return sendDataResponse(res, 200, currs)
+    return sendDataResponse(res, 200, currs)
+  } catch (err) {
+    const error = new ServerErrorEvent(req.user, 'fetch-all-curriculums')
+    myEmitter.emit('error', error)
+    return sendMessageResponse(res, error.code, error.message)
+  }
 }
