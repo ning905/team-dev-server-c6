fd0c422fd690a20515eefeeab6b15ec692b10d47
Ning Ma <ningma905@gmail.com>
2022-11-09 19:26:58 +0000

    for deployment

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { login } from '../controllers/auth.js'
+import { sendMessageResponse } from '../utils/responses.js'
 
 const router = Router()
 
+router.get('/', (req, res) => sendMessageResponse(res, 200, 'OK'))
 router.post('/login', login)
 
 export default router
